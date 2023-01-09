1422e5758229fc7e2aeac109b04dd5cc1954ff30
Joel Mandell <joelmandell@gmail.com>
2023-01-10 00:20:16 +0100

    Added support for json_extract for Sqlite provider (DbFunction).

---
+++ src/EFCore.Sqlite.Core/Query/Internal/SqliteJsonFunctionsTranslator.cs
@@ -0,0 +1,76 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+
+namespace Microsoft.EntityFrameworkCore.Sqlite.Query.Internal;
+
+/// <summary>
+///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+///     any release. You should only use it directly in your code with extreme caution and knowing that
+///     doing so can result in application failures when updating to a new Entity Framework Core release.
+/// </summary>
+public class SqliteJsonFunctionsTranslator : IMethodCallTranslator
+{
+
+    private readonly Dictionary<MethodInfo, string> _methodInfoJsonFunctions
+        = new()
+        {
+            {
+                typeof(SqliteDbFunctionsExtensions).GetRuntimeMethod(nameof(SqliteDbFunctionsExtensions.JsonExtract), new[] { typeof(DbFunctions), typeof(object), typeof(string) })!,
+                "json_extract"
+            }
+        };
+
+    private readonly ISqlExpressionFactory _sqlExpressionFactory;
+
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    public SqliteJsonFunctionsTranslator(
+        ISqlExpressionFactory sqlExpressionFactory)
+    {
+        _sqlExpressionFactory = sqlExpressionFactory;
+    }
+
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    public SqlExpression? Translate(
+        SqlExpression? instance,
+        MethodInfo method,
+        IReadOnlyList<SqlExpression> arguments,
+        IDiagnosticsLogger<DbLoggerCategory.Query> logger)
+    {
+        if (_methodInfoJsonFunctions.TryGetValue(method, out var function))
+        {
+            var propertyReference = arguments[1];
+
+            var typeMapping = propertyReference.TypeMapping;
+
+            var path = propertyReference.Type == arguments[2].Type
+                        ? _sqlExpressionFactory.ApplyTypeMapping(arguments[2], typeMapping)
+                        : _sqlExpressionFactory.ApplyDefaultTypeMapping(arguments[2]);
+
+            var functionArguments = new List<SqlExpression> { propertyReference, path };
+
+            return _sqlExpressionFactory.Function(
+                function,
+                functionArguments,
+                nullable: true,
+                // TODO: don't propagate for now
+                argumentsPropagateNullability: functionArguments.Select(_ => false).ToList(),
+                typeof(string));
+        }
+
+        return null;
+    }
+}
