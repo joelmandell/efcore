938bbcb95c93ea0226567b4ad741e2bc08559263
Joel Mandell <joelmandell@gmail.com>
2023-03-29 04:52:38 +0200

    Update src/EFCore.SqlServer/Query/Internal/SqlServerJsonFunctionsTranslator.cs
    Co-authored-by: Shay Rojansky <roji@roji.org>

--- src/EFCore.SqlServer/Query/Internal/SqlServerJsonFunctionsTranslator.cs
+++ src/EFCore.SqlServer/Query/Internal/SqlServerJsonFunctionsTranslator.cs
@@ -56,7 +56,6 @@ public class SqlServerJsonFunctionsTranslator : IMethodCallTranslator
         if (_methodInfoJsonFunctions.TryGetValue(method, out var function))
         {
             var expression = arguments[1];
-
             var path = arguments[2];
 
             //Do not allow null as path arg. Pass '$' as default path.
