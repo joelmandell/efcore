5b2ae4e241f32168265a7a664bdeba71438a94cd
Joel Mandell <joelmandell@gmail.com>
2023-07-14 23:31:21 +0200

    Json_Extract overload. Fix formating. Move tests to correct place.

--- src/EFCore.Sqlite.Core/Query/Internal/SqliteJsonFunctionsTranslator.cs
+++ src/EFCore.Sqlite.Core/Query/Internal/SqliteJsonFunctionsTranslator.cs
@@ -12,12 +12,21 @@ namespace Microsoft.EntityFrameworkCore.Sqlite.Query.Internal;
 ///     doing so can result in application failures when updating to a new Entity Framework Core release.
 /// </summary>
 public class SqliteJsonFunctionsTranslator : IMethodCallTranslator
-{
-    private static MethodInfo _methodInfoJsonExtract =>
-        typeof(SqliteDbFunctionsExtensions).GetRuntimeMethod(nameof(SqliteDbFunctionsExtensions.JsonExtract), new[] { typeof(DbFunctions), typeof(object), typeof(string[]) })!;
+{ 
+    private static readonly Dictionary<MethodInfo, string> _methodInfoJsonFunctions
+        = new()
+        {
+            {
+                typeof(SqliteDbFunctionsExtensions).GetRuntimeMethod(nameof(SqliteDbFunctionsExtensions.JsonExtract), new[] { typeof(DbFunctions), typeof(object), typeof(string[]) })!,
+                "JsonExtract_Paths"
+            },
+            {
+                typeof(SqliteDbFunctionsExtensions).GetRuntimeMethod(nameof(SqliteDbFunctionsExtensions.JsonExtract), new[] { typeof(DbFunctions), typeof(object), typeof(string) })!,
+                "JsonExtract_Path"
+            },
+        };
 
     private readonly ISqlExpressionFactory _sqlExpressionFactory;
-
 
     /// <summary>
     ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
@@ -43,25 +52,34 @@ public class SqliteJsonFunctionsTranslator : IMethodCallTranslator
         IReadOnlyList<SqlExpression> arguments,
         IDiagnosticsLogger<DbLoggerCategory.Query> logger)
     {
-        if (method == _methodInfoJsonExtract)
+        if(_methodInfoJsonFunctions.TryGetValue(method,out _))
         {
             var expression = arguments[1];
-            var paths = arguments[2];
+            var pathArg = arguments[2];
             var functionArguments = new List<SqlExpression> { expression };
-            var pathValue = ((SqlConstantExpression)paths)?.Value;
+            var pathValue = ((SqlConstantExpression)pathArg)?.Value;
 
-            if (pathValue?.GetType() == typeof(string[]))
+            //If return type is of string it means JSON_EXTRACT will return text which is a well-formed JSON array holding the various values.
+            if (method.ReturnParameter?.ParameterType == typeof(string))
             {
-                var constantValues = pathValue is not null ? (string[])pathValue : Array.Empty<string>();
-
-                foreach (var path in constantValues)
+                if (pathValue?.GetType() == typeof(string[]))
                 {
-                    functionArguments.Add(_sqlExpressionFactory.Constant(path));
+                    var constantValues = pathValue is not null ? (string[])pathValue : Array.Empty<string>();
+
+                    foreach (var path in constantValues)
+                    {
+                        functionArguments.Add(_sqlExpressionFactory.Constant(path));
+                    }
                 }
             }
 
+            //If return type is of object it means JSON_EXTRACT will return either null, INTEGER or REAL for a JSON numeric value, an INTEGER zero for a JSON false value, an INTEGER one for a JSON true value.
+            //the dequoted text for a JSON string value, and a text representation for JSON object and array values.
+            if (method.ReturnParameter?.ParameterType == typeof(object))
+                functionArguments.Add(_sqlExpressionFactory.Constant(pathValue));
+
             return _sqlExpressionFactory.Function(
-                nameof(SqliteDbFunctionsExtensions.JsonExtract),
+                "JSON_EXTRACT",
                 functionArguments,
                 nullable: true,
                 argumentsPropagateNullability: functionArguments.Select(_ => true).ToList(),
